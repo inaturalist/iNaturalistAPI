1abc7a31915e6e5ed57481a139d581c1eeb519ac
Patrick Leary <patrick.r.leary@gmail.com>
2016-03-07 16:47:52 -0500

    consolidating model logic; more fields returned in obs details

--- test/models/user.js
+++ test/models/user.js
@@ -39,20 +39,6 @@ describe( "User", function( ) {
     });
   });
 
-  describe( "assignToObject", function( ) {
-    it( "assigns user instances to objects", function( done ) {
-      var o = { 1: { }, 123: { }, 444: { } };
-      User.assignToObject( o, function( err, withUsers ) {
-        expect( _.keys( withUsers ) ).to.deep.eq([ "1", "123", "444" ]);
-        expect( withUsers[ "1" ].user ).to.be.undefined;
-        expect( withUsers[ "444" ].user ).to.be.undefined;
-        expect( withUsers[ "123" ].user ).to.not.be.undefined;
-        expect( withUsers[ "123" ].user.login ).to.eq( "a-user" );
-        done( );
-      });
-    });
-  });
-
   describe( "iconUrl", function( ) {
     it( "returns nothing if there is no icon", function( ) {
       expect( User.iconUrl({ }) ).to.be.undefined;
