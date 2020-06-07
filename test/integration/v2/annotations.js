6a11007734a1cf7c63d7740662f4ed8df2e103ca
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2020-06-06 18:24:21 -0700

    Added create and delete for annotations v2

--- test/integration/v2/annotations.js
+++ test/integration/v2/annotations.js
@@ -1,4 +1,4 @@
-// const { expect } = require( "chai" );
+const { expect } = require( "chai" );
 const _ = require( "lodash" );
 const request = require( "supertest" );
 const nock = require( "nock" );
@@ -9,10 +9,49 @@ const app = require( "../../../openapi_app" );
 
 const fixtures = JSON.parse( fs.readFileSync( "schema/fixtures.js" ) );
 
-describe( "Annotations", ( ) => {
+describe.only( "Annotations", ( ) => {
+  const a = {
+    id: 123,
+    uuid: "dcd68b65-e5ae-4581-8cea-5788cbefe53d"
+  };
+  const token = jwt.sign( { user_id: 333 }, config.jwtSecret || "secret",
+    { algorithm: "HS512" } );
+  describe( "create", ( ) => {
+    it( "returns JSON", done => {
+      nock( "http://localhost:3000" )
+        .post( "/annotations" )
+        .reply( 200, { id: a.id, uuid: a.uuid } );
+      request( app ).post( "/v2/annotations" )
+        .set( "Authorization", token )
+        .set( "Content-Type", "multipart/form-data" )
+        // it doesn't really matter what we post since we're just stubbing the
+        // Rails app to return obs 6 to load from the ES index
+        .field( "comment", JSON.stringify( { } ) )
+        .expect( 200 )
+        .expect( res => {
+          const anno = res.body.results[0];
+          expect( anno.uuid ).to.eq( a.uuid );
+        } )
+        .expect( "Content-Type", /json/ )
+        .expect( 200, done );
+    } );
+  } );
+  describe( "delete", ( ) => {
+    it( "should not return anything if successful", done => {
+      nock( "http://localhost:3000" )
+        .delete( `/annotations/${a.uuid}` )
+        .reply( 200 );
+      request( app ).delete( `/v2/annotations/${a.uuid}` )
+        .set( "Authorization", token )
+        .set( "Content-Type", "application/json" )
+        .expect( 200 )
+        .expect( res => {
+          expect( res.body ).to.eq( "" );
+        } )
+        .expect( 200, done );
+    } );
+  } );
   describe( "voting", ( ) => {
-    const token = jwt.sign( { user_id: 333 }, config.jwtSecret || "secret",
-      { algorithm: "HS512" } );
     const obs = _.find(
       fixtures.elasticsearch.observations.observation,
       o => o.annotations && o.annotations.length > 0
