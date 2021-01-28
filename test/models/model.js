754e410db4ad25498aebe51bcf881583db52f4a0
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2021-01-27 17:01:03 -0800

    Only normalize date strings if the attribute looks like it contains a datetime

--- test/models/model.js
+++ test/models/model.js
@@ -9,10 +9,19 @@ describe( "Model", ( ) => {
     } );
 
     it( "uses a consistent format for dates", ( ) => {
-      let m = new Model( { d: new Date( "2016-01-01 00:00:00 +00:00" ) } );
-      expect( m.d ).to.eq( "2016-01-01T00:00:00+00:00" );
-      m = new Model( { d: "2016-01-01T00:00:00" } );
-      expect( m.d ).to.eq( "2016-01-01T00:00:00+00:00" );
+      let m = new Model( { updated_at: new Date( "2016-01-01 00:00:00 +00:00" ) } );
+      expect( m.updated_at ).to.eq( "2016-01-01T00:00:00+00:00" );
+      m = new Model( { updated_at: "2016-01-01T00:00:00" } );
+      expect( m.updated_at ).to.eq( "2016-01-01T00:00:00+00:00" );
+    } );
+
+    it( "does not parse dates in fields that don't look like dates", ( ) => {
+      const body1 = "2016-01-01 00:00:00 +00:00";
+      let m = new Model( { body: body1 } );
+      expect( m.body ).to.eq( body1 );
+      const body2 = "2016-01-01T00:00:00";
+      m = new Model( { body: body2 } );
+      expect( m.body ).to.eq( body2 );
     } );
   } );
 } );
