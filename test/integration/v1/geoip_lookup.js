2d6d8aeb4d6d704cbbd8127d152a049b83518572
Patrick Leary <patrick.r.leary@gmail.com>
2021-07-26 14:42:20 -0400

    geoip requests require authorization

--- test/integration/v1/geoip_lookup.js
+++ test/integration/v1/geoip_lookup.js
@@ -1,20 +1,38 @@
 const { expect } = require( "chai" );
 const request = require( "supertest" );
+const jwt = require( "jsonwebtoken" );
 const iNaturalistAPI = require( "../../../lib/inaturalist_api" );
+const config = require( "../../../config.js" );
 
 const app = iNaturalistAPI.server( );
 
 describe( "geoip_lookup", ( ) => {
   describe( "ping", ( ) => {
-    it( "returns geoip responses", done => {
+    it( "fails without a token", done => {
       request( app ).get( "/v1/geoip_lookup?ip=128.128.128.128" )
+        .set( "Authorization", "" )
+        .expect( 401, done );
+    } );
+
+    it( "fails without a valid token", done => {
+      const token = jwt.sign( { application: "whatever" }, "not the right secret",
+        { algorithm: "HS512" } );
+      request( app ).get( "/v1/geoip_lookup?ip=128.128.128.128" )
+        .set( "Authorization", token )
+        .expect( 401, done );
+    } );
+
+    it( "returns geoip responses", done => {
+      const token = jwt.sign( { application: "whatever" }, config.jwtApplicationSecret || "appsecret",
+        { algorithm: "HS512" } );
+      request( app ).get( "/v1/geoip_lookup?ip=128.128.128.128" )
+        .set( "Authorization", token )
         .expect( res => {
-          expect( res.body.results.country ).to.eq( "US" );
-          expect( res.body.results.ll[0] ).to.be.above( 41 );
-          expect( res.body.results.ll[0] ).to.be.below( 42 );
-          expect( res.body.results.ll[1] ).to.be.above( -71 );
-          expect( res.body.results.ll[1] ).to.be.below( -70 );
-        } ).expect( "Content-Type", /json/ )
+          expect( res.body.results.country ).to.not.be.undefined;
+          expect( res.body.results.ll[0] ).to.be.within( -90, 90 );
+          expect( res.body.results.ll[1] ).to.be.within( -180, 180 );
+        } )
+        .expect( "Content-Type", /json/ )
         .expect( 200, done );
     } );
   } );
