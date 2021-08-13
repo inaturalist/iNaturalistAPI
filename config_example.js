ae870cb26a6122c9d63323050ed8c4e1141732da
Sean Clifford <1187769+seanclifford@users.noreply.github.com>
2021-08-14 11:18:06 +1200

    Tidy jwt secret fallbacks and example config
    Equivalent change planned for inaturalist example config

--- config_example.js
+++ config_example.js
@@ -46,8 +46,8 @@ module.exports = {
   debug: true,
   staticImagePrefix: "http://localhost:3000/attachments/",
   userImagePrefix: "/attachments/users/icons/",
-  jwtSecret: "jwtSecret",
-  jwtApplicationSecret: "jwtApplicationSecret",
+  jwtSecret: "secret",
+  jwtApplicationSecret: "application_secret",
   imageProcesing: {
     // Path to a file listing the taxonomy used in the computer vision model
     taxaFilePath: "",
