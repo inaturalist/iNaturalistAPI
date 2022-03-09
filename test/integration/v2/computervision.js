a78dcb4e357dc85837ad1f06f74769660bb18168
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-03-08 18:26:15 -0800

    Added v2/computervision/score_image

--- test/integration/v2/computervision.js
+++ test/integration/v2/computervision.js
@@ -4,7 +4,7 @@ const fs = require( "fs" );
 const request = require( "supertest" );
 const nock = require( "nock" );
 const jwt = require( "jsonwebtoken" );
-const config = require( "../../../config.js" );
+const config = require( "../../../config" );
 const app = require( "../../../app" );
 
 const fixtures = JSON.parse( fs.readFileSync( "schema/fixtures.js" ) );
@@ -24,8 +24,11 @@ describe( "Computervision", ( ) => {
         .reply( 200, fakeVisionResults );
     } );
     it( "returns JSON", done => {
-      const token = jwt.sign( { user_id: 333 }, config.jwtSecret || "secret",
-        { algorithm: "HS512" } );
+      const token = jwt.sign(
+        { user_id: 333 },
+        config.jwtSecret || "secret",
+        { algorithm: "HS512" }
+      );
       request( app ).get( url )
         .set( "Authorization", token )
         .expect( 200 )
@@ -36,8 +39,11 @@ describe( "Computervision", ( ) => {
         .expect( 200, done );
     } );
     it( "works with an application token", done => {
-      const token = jwt.sign( { application: "whatever" }, config.jwtApplicationSecret || "application_secret",
-        { algorithm: "HS512" } );
+      const token = jwt.sign(
+        { application: "whatever" },
+        config.jwtApplicationSecret || "application_secret",
+        { algorithm: "HS512" }
+      );
       request( app ).get( url )
         .set( "Authorization", token )
         .expect( 200, done );
@@ -48,18 +54,50 @@ describe( "Computervision", ( ) => {
         .expect( 401, done );
     } );
     it( "should fail without a valid token", done => {
-      const token = jwt.sign( { application: "whatever" }, "not the right secret",
-        { algorithm: "HS512" } );
+      const token = jwt.sign(
+        { application: "whatever" },
+        "not the right secret",
+        { algorithm: "HS512" }
+      );
       request( app ).get( url )
         .set( "Authorization", token )
         .expect( 401, done );
     } );
     it( "should fail without a token that specifies a user or an application", done => {
-      const token = jwt.sign( { thisIs: "the way" }, config.jwtApplicationSecret || "application_secret",
-        { algorithm: "HS512" } );
+      const token = jwt.sign(
+        { thisIs: "the way" },
+        config.jwtApplicationSecret || "application_secret",
+        { algorithm: "HS512" }
+      );
       request( app ).get( url )
         .set( "Authorization", token )
         .expect( 401, done );
+    } );
+  } );
+  describe( "score_image", ( ) => {
+    beforeEach( ( ) => {
+      const fakeVisionResults = { 1: 0.9, 2: 0.1 };
+      nock( config.imageProcesing.tensorappURL )
+        .post( "/" )
+        .reply( 200, fakeVisionResults );
+    } );
+    it( "returns JSON", done => {
+      const token = jwt.sign(
+        { user_id: 333 },
+        config.jwtSecret || "secret",
+        { algorithm: "HS512" }
+      );
+      request( app ).post( "/v2/computervision/score_image" )
+        .set( "Authorization", token )
+        .set( "Content-Type", "multipart/form-data" )
+        .field( "fields", "id,uuid" )
+        .attach( "image", "test/fixtures/cuthona_abronia-tagged.jpg" )
+        .expect( 200 )
+        .expect( res => {
+          expect( res.body.results.length ).to.be.above( 0 );
+        } )
+        .expect( "Content-Type", /json/ )
+        .expect( 200, done );
     } );
   } );
 } );
