3943906d04f57d46faed30c08daa1ce031074bce
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-03-11 12:44:15 -0800

    Ensure description and other fields get returned for /v2/users/:id
    The description attribute is not in the ES index, so our fixtures were
    actually incorrect. This fixes the fixtures and adds a new implementation
    of /v2/users/:id that loads data from the db and from ES every time, and can
    return all the in the documented response schema... which itself was wildly
    inaccurate as it included all of the private info returned by /v2/users/me,
    so I trimmed that down and made a separate schema for that.

--- test/integration/v2/users.js
+++ test/integration/v2/users.js
@@ -3,12 +3,27 @@ const fs = require( "fs" );
 const jwt = require( "jsonwebtoken" );
 const nock = require( "nock" );
 const request = require( "supertest" );
-const config = require( "../../../config.js" );
+const { expect } = require( "chai" );
+const config = require( "../../../config" );
 const app = require( "../../../app" );
 
 const fixtures = JSON.parse( fs.readFileSync( "schema/fixtures.js" ) );
 
 describe( "Users", ( ) => {
+  describe( "show", ( ) => {
+    it( "should include description when requested", done => {
+      const user = _.find(
+        fixtures.postgresql.users,
+        u => u.description && u.description.length > 0
+      );
+      request( app ).get( `/v2/users/${user.id}?fields=description` )
+        .expect( 200 )
+        .expect( res => {
+          expect( res.body.results[0].description ).to.eq( user.description );
+        } )
+        .expect( 200, done );
+    } );
+  } );
   describe( "update_session", ( ) => {
     it( "should fail without auth", done => {
       request( app ).put( "/v2/users/update_session" )
@@ -26,8 +41,11 @@ describe( "Users", ( ) => {
 
     it( "should return JSON with auth", done => {
       const currentUser = fixtures.elasticsearch.users.user[0];
-      const token = jwt.sign( { user_id: currentUser.id }, config.jwtSecret || "secret",
-        { algorithm: "HS512" } );
+      const token = jwt.sign(
+        { user_id: currentUser.id },
+        config.jwtSecret || "secret",
+        { algorithm: "HS512" }
+      );
       nock( "http://localhost:3000" )
         .put( "/users/update_session" )
         .reply( 204 );
