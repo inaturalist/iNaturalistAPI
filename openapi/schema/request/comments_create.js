fb5bd1d81318927e79b7d6c5a134d5b81eb16d45
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2022-03-14 13:24:54 -0700

    Update request schemas for comments and IDs
    * Allow comment creation to specify a parent
    * Force identification createion to use obs UUID
    * Specify required fields
    * Make DELETE /v2/identifications/:id actually delete the record; withdraw is
      an UPDATE

--- openapi/schema/request/comments_create.js
+++ openapi/schema/request/comments_create.js
@@ -3,6 +3,18 @@ const Joi = require( "joi" );
 module.exports = Joi.object( ).keys( {
   fields: Joi.any( ),
   comment: Joi.object( ).keys( {
-    body: Joi.string( )
-  } )
+    parent_type: Joi
+      .string( )
+      .valid(
+        "Observation",
+        "Post"
+      )
+      .description( "Type of record being commented on" )
+      .required( ),
+    parent_id: Joi
+      .string( ).guid( )
+      .required( )
+      .description( "UUID of record being commented on" ),
+    body: Joi.string( ).required( )
+  } ).required( )
 } );
