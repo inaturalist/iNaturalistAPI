b54b32f5a3c36bb026f2c5e9018f82ac9dfa1ec8
sylvain-morin <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>
2023-02-15 14:31:41 +0100

    Allow to override dbname with INAT_DB_NAME env value (except for test)

--- test/_initialize.js
+++ test/_initialize.js
@@ -9,16 +9,17 @@ console.log( "INITIALIZING TEST ENVIRONMENT\n" );
 // test_helper, so here we're setting up the database *before* we require
 // anything that would require those modules. Naming this file _initialize.js
 // ensures mocha runs this file first when running tests.
+const dbname = "inaturalist_test";
 const testDbConnectionVar = `${config.database.host ? `PGHOST=${config.database.host}` : ""} \
                              ${config.database.user ? `PGUSER=${config.database.user}` : ""} \
                              ${config.database.password ? `PGPASSWORD=${config.database.password}` : ""}`;
 
 console.log( "Dropping existing test database" );
-execSync( `${testDbConnectionVar} dropdb --if-exists ${config.database.dbname}`, { stdio: [0, 1, 2] } );
+execSync( `${testDbConnectionVar} dropdb --if-exists ${dbname}`, { stdio: [0, 1, 2] } );
 console.log( "Creating test database" );
-execSync( `${testDbConnectionVar} createdb -O ${config.database.user} ${config.database.dbname}`, { stdio: [0, 1, 2] } );
+execSync( `${testDbConnectionVar} createdb -O ${config.database.user} ${dbname}`, { stdio: [0, 1, 2] } );
 console.log( "Loading test database schema" );
-execSync( `${testDbConnectionVar} psql -q -f schema/database.sql -d ${config.database.dbname}`, { stdio: [0, 1, 2] } );
+execSync( `${testDbConnectionVar} psql -q -f schema/database.sql -d ${dbname}`, { stdio: [0, 1, 2] } );
 
 /* eslint import/order: 0 */
 const testHelper = require( "../lib/test_helper" );
