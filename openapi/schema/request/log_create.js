60efad269e43efdc253e70d1537ef6cc305e5049
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2023-04-05 18:10:00 -0700

    Use context, level, and client_timestamp logstash fields

--- openapi/schema/request/log_create.js
+++ openapi/schema/request/log_create.js
@@ -7,9 +7,10 @@ module.exports = Joi.object( ).keys( {
     "error",
     "warn"
   ).description( "Level of log event" ),
-  message: Joi.string( ),
-  context: Joi.string( ),
-  error_type: Joi.string( ),
+  message: Joi.string( ).description( "What happened that was worth logging" ),
+  context: Joi.string( ).description( "Brief description of where the log event came from, e.g. a file or class name" ),
+  timestamp: Joi.date( ).description( "Datetime the event was logged in the client in case of asynchronous logging" ),
+  error_type: Joi.string( ).description( "Type of error, e.g. java.lang.NoClassDefFoundError" ),
   backtrace: Joi.string( )
 } ).description(
   "Log an event that occurred in a client application"
