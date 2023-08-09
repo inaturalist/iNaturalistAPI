d02fe89127a2a55741e40ccd88491fb5a1813b51
sylvain-morin <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>
2023-08-09 12:59:24 +0200

    Prepare Dockerfile for local env

--- config_example.js
+++ config_example.js
@@ -7,14 +7,17 @@ const {
   INAT_WEB_HOST
 } = process.env;
 module.exports = {
-  // Base URL for the current version of *this* app
-  currentVersionURL: "http://localhost:4000/v1",
   // Host running the iNaturalist Rails app
   apiURL: INAT_WEB_HOST ? `http://${INAT_WEB_HOST}:3000` : "http://localhost:3000",
-  jwtSecret: "secret",
-  jwtApplicationSecret: "application_secret",
-  tileSize: 512,
-  debug: true,
+  // Base URL for the current version of *this* app
+  currentVersionURL: "http://localhost:4000/v1",
+  // Whether the Rails app supports SSL requests. For local dev assume it does not
+  apiHostSSL: false,
+  writeHostSSL: false,
+  elasticsearch: {
+    host: INAT_ES_HOST ? `http://${INAT_ES_HOST}:9200` : "http://localhost:9200",
+    geoPointField: "location"
+  },
   database: {
     user: INAT_DB_USER || "inaturalist",
     host: INAT_DB_HOST || "127.0.0.1",
@@ -24,28 +27,25 @@ module.exports = {
     password: INAT_DB_PASS || "inaturalist",
     ssl: false
   },
-  staticImagePrefix: "http://localhost:3000/attachments/",
+  tileSize: 512,
+  debug: true,
+  logLevel: "info", // only "debug" does anything right now
   websiteURL: "http://localhost:3000/",
+  staticImagePrefix: "http://localhost:3000/attachments/",
+  userImagePrefix: "/attachments/users/icons/",
+  jwtSecret: "secret",
+  jwtApplicationSecret: "application_secret",
   imageProcesing: {
-    // Base URL for the web app returning computer vision results
-    tensorappURL: "http://localhost:6006",
+    // Path to a file listing the taxonomy used in the computer vision model
+    taxaFilePath: "",
     // Path to a directory where uploads should be stored. /tmp/ is fine on most
     // *nix systems
     uploadsDir: "/tmp/",
-    // Path to a file listing the taxonomy used in the computer vision model
-    taxaFilePath: ""
+    // Base URL for the web app returning computer vision results
+    tensorappURL: "http://localhost:6006"
   },
   redis: {
     host: INAT_REDIS_HOST || "127.0.0.1",
     port: 6379
-  },
-  elasticsearch: {
-    host: INAT_ES_HOST ? `http://${INAT_ES_HOST}:9200` : "http://localhost:9200",
-    geoPointField: "location"
-  },
-  // Whether the Rails app supports SSL requests. For local dev assume it does not
-  apiHostSSL: false,
-  writeHostSSL: false,
-  logLevel: "info", // only "debug" does anything right now
-  userImagePrefix: "/attachments/users/icons/"
+  }
 };
