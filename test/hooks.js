7e6e4dbb0aa5b98904d09cf727bf814e5e8ffb22
Patrick Leary <patrick.r.leary@gmail.com>
2023-09-19 15:35:39 -0400

    remove remaining use of config.environment; fix orcid parameter regex

--- test/hooks.js
+++ test/hooks.js
@@ -1,7 +1,9 @@
+const { expect } = require( "chai" );
 const app = require( "../app" );
 
 exports.mochaHooks = {
   async beforeAll( ) {
+    expect( process.env.NODE_ENV ).to.eq( "test" );
     this.timeout( 20000 );
     this.app = await app( );
   }
