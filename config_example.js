edec8eda39dcfa8e541afeb4e15c833a12556564
sylvain-morin <sylvain.morin@inaturalist.org>
2024-01-29 18:19:21 +0100

    Use ENV variable for docker configuration

--- config_example.js
+++ config_example.js
@@ -6,15 +6,17 @@ const {
   INAT_DB_PASS,
   INAT_ES_HOST,
   INAT_REDIS_HOST,
-  INAT_WEB_HOST,
+  INAT_API_URL,
+  INAT_RAILS_URL,
+  INAT_VISION_URL,
   INAT_DB_SSL_KEY_PATH,
   INAT_DB_SSL_CRT_PATH
 } = process.env;
 module.exports = {
   // Host running the iNaturalist Rails app
-  apiURL: INAT_WEB_HOST ? `http://${INAT_WEB_HOST}:3000` : "http://localhost:3000",
+  apiURL: INAT_RAILS_URL ? `${INAT_RAILS_URL}` : "http://localhost:3000",
   // Base URL for the current version of *this* app
-  currentVersionURL: "http://localhost:4000/v1",
+  currentVersionURL: INAT_API_URL ? `${INAT_API_URL}` : "http://localhost:4000/v1",
   // Whether the Rails app supports SSL requests. For local dev assume it does not
   apiHostSSL: false,
   writeHostSSL: false,
@@ -50,7 +52,7 @@ module.exports = {
     // *nix systems
     uploadsDir: "/tmp/",
     // Base URL for the web app returning computer vision results
-    tensorappURL: "http://localhost:6006"
+    tensorappURL: INAT_VISION_URL ? `${INAT_VISION_URL}` : "http://localhost:6006"
   },
   redis: {
     host: INAT_REDIS_HOST || "127.0.0.1",
