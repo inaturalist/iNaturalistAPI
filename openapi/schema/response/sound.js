bc1ea618ab8557db6cbb1506c84b8606864b74ce
Patrick Leary <patrick.r.leary@gmail.com>
2024-02-26 11:12:07 -0500

    v2 returns hidden flag and moderator_actions with photos and sounds; spec fix

--- openapi/schema/response/sound.js
+++ openapi/schema/response/sound.js
@@ -1,13 +1,16 @@
 const Joi = require( "joi" );
 const flag = require( "./flag" );
+const moderatorAction = require( "./moderator_action" );
 
 module.exports = Joi.object( ).keys( {
   attribution: Joi.string( ),
   file_content_type: Joi.string( ),
   file_url: Joi.string( ),
   flags: Joi.array( ).items( flag ),
+  hidden: Joi.boolean( ),
   id: Joi.number( ).integer( ).description( "Unique auto-increment integer identifier." ).required( ),
   license_code: Joi.string( ).valid( null ),
+  moderator_actions: Joi.array( ).items( moderatorAction ),
   native_sound_id: Joi.string( ).valid( null )
 } ).unknown( false ).meta( { className: "Sound" } )
   .valid( null );
