8997cf51a9ed92031274c676e353f4209c6859cf
Patrick Leary <patrick.r.leary@gmail.com>
2024-05-28 10:24:38 -0400

    v2 photo and sound create accept uuid for upsert inaturalist/inaturalist#4078; update geoip-lite

--- openapi/schema/request/sounds_create.js
+++ openapi/schema/request/sounds_create.js
@@ -2,5 +2,6 @@ const Joi = require( "joi" );
 
 module.exports = Joi.object( ).keys( {
   file: Joi.binary( ).required( ),
-  fields: Joi.any( )
+  fields: Joi.any( ),
+  uuid: Joi.string( ).guid( { version: "uuidv4" } )
 } ).unknown( true );
