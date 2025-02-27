0dc50b22b7ea13af7100485fbfbb1963f1b6451e
Patrick Leary <patrick.r.leary@gmail.com>
2025-02-27 13:52:55 -0500

    log endpoint extra attributes added to logstasher (#497)

--- openapi/schema/request/log_create.js
+++ openapi/schema/request/log_create.js
@@ -11,7 +11,15 @@ module.exports = Joi.object( ).keys( {
   context: Joi.string( ).description( "Brief description of where the log event came from, e.g. a file or class name" ),
   timestamp: Joi.date( ).description( "Datetime the event was logged in the client in case of asynchronous logging" ),
   error_type: Joi.string( ).description( "Type of error, e.g. java.lang.NoClassDefFoundError. Only used when level is error" ),
-  backtrace: Joi.string( ).description( "Only used when level is error" )
+  backtrace: Joi.string( ).description( "Only used when level is error" ),
+  extra: Joi.object( ).pattern(
+    Joi.string( ),
+    Joi.alternatives( ).try(
+      Joi.string( ),
+      Joi.number( ),
+      Joi.boolean( )
+    )
+  ).description( "Accepts key/values where the values are not objects, and logs the contents" )
 } ).description(
   "Log an event that occurred in a client application"
 ).meta( { unpublished: true } );
