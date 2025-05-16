d34938ed81902b59d2e2a1942b670e198f876a6a
Patrick Leary <patrick.r.leary@gmail.com>
2025-05-16 13:35:42 -0400

    feat: users autocomplete can return suspended users if requested, related to WEB-706

--- openapi/schema/request/users_autocomplete.js
+++ openapi/schema/request/users_autocomplete.js
@@ -2,6 +2,7 @@ const Joi = require( "joi" );
 
 module.exports = Joi.object( ).keys( {
   q: Joi.string( ),
+  include_suspended: Joi.boolean( ).default( false ),
   per_page: Joi.number( ).integer( ),
   fields: Joi.any( )
 } );
