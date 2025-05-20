8fe3739b3133972a705b0aff008797f020c9484b
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2025-05-20 12:32:48 -0700

    fix: allow updating profile pic when faved projects chosen
    Allows unknown attributes in multipart PUT /v2/users/:id requests. This allows
    this endpoint to receive multipart attributes like users[faved_project_ids]
    [0] even though we don't specify that as an allowed attribute. The root of
    the problem is converting a Joi schema that expects a JSON array to one that
    expects multipart FormData. There may be a better way to do this using Joi's
    pattern() option.

--- openapi/schema/request/users_update_multipart.js
+++ openapi/schema/request/users_update_multipart.js
@@ -2,6 +2,10 @@ const Joi = require( "joi" );
 const usersUpdate = require( "./users_update" );
 const openapiUtil = require( "../../openapi_util" );
 
-module.exports = openapiUtil.applicationJsonToMultipart( usersUpdate ).append( {
-  "user[icon]": Joi.binary( )
-} );
+// Convert the JSON schema to equivalent FormData keys, e.g. user[description]
+module.exports = openapiUtil.applicationJsonToMultipart( usersUpdate )
+  .append( { "user[icon]": Joi.binary( ) } )
+  // Unfortunately, array items will be submitted in FormData as
+  // user[some_array_attr][0], and we don't know of a way to specify
+  // wildcards in keys
+  .unknown( true );
