1be41fc5e2fddb83a3daa3242621f13b5c7e72fb
Patrick Leary <patrick.r.leary@gmail.com>
2025-12-05 13:51:39 -0500

    fix: place schema in conservation status responses, WEB-862 (#554)

--- openapi/schema/response/conservation_status.js
+++ openapi/schema/response/conservation_status.js
@@ -1,5 +1,4 @@
 const Joi = require( "joi" );
-const place = require( "./place" );
 
 module.exports = Joi.object( ).keys( {
   id: Joi.number( ).integer( ).required( ),
@@ -9,7 +8,12 @@ module.exports = Joi.object( ).keys( {
   iucn: Joi.number( ).integer( ),
   iucn_status: Joi.string( ),
   iucn_status_code: Joi.string( ),
-  place,
+  place: Joi.object( {
+    id: Joi.number( ).integer( ),
+    name: Joi.string( ),
+    display_name: Joi.string( ),
+    ancestry: Joi.string( ).valid( null )
+  } ),
   place_id: Joi.number( ).integer( ).valid( null ),
   source_id: Joi.number( ).integer( ).valid( null ),
   status: Joi.string( ),
