ad5eeeb1a30a91d149d8575e9f7a00b6b3294efd
Patrick Leary <patrick.r.leary@gmail.com>
2026-03-17 11:12:29 -0400

    feat: update access requirements for exemplar identifications, WEB-939 (#591)

--- test/integration/v2/exemplar_identifications.js
+++ test/integration/v2/exemplar_identifications.js
@@ -1,18 +1,68 @@
 const { expect } = require( "chai" );
 const request = require( "supertest" );
 const jwt = require( "jsonwebtoken" );
+const sinon = require( "sinon" );
+const UserSession = require( "../../../lib/user_session" );
 const config = require( "../../../config" );
 
 describe( "ExemplarIdentifications", ( ) => {
   describe( "search", ( ) => {
-    const token = jwt.sign(
+    const adminToken = jwt.sign(
       { user_id: 1 },
       config.jwtSecret || "secret",
       { algorithm: "HS512" }
     );
+
+    const nonAdminToken = jwt.sign(
+      { user_id: 123 },
+      config.jwtSecret || "secret",
+      { algorithm: "HS512" }
+    );
+
+    it( "does not allow unauthenticated requests", function ( done ) {
+      request( this.app ).get( "/v2/exemplar_identifications" )
+        .set( "Content-Type", "application/json" )
+        .expect( 401, done );
+    } );
+
+    it( "does not allow non-admins that are not in relevant test groups", function ( done ) {
+      request( this.app ).get( "/v2/exemplar_identifications" )
+        .set( "Authorization", nonAdminToken )
+        .set( "Content-Type", "application/json" )
+        .expect( 401, done );
+    } );
+
+    it( "does not allow non-admins that not in all relevant test groups", function ( done ) {
+      sinon.stub( UserSession.prototype, "extend" )
+        .callsFake( function ( ) {
+          this.test_groups = ["helpful-id-tips"];
+        } );
+      request( this.app ).get( "/v2/exemplar_identifications" )
+        .set( "Authorization", nonAdminToken )
+        .set( "Content-Type", "application/json" )
+        .expect( 401, ( ) => {
+          UserSession.prototype.extend.restore( );
+          done( );
+        } );
+    } );
+
+    it( "allows non-admins that are in relevant test groups", function ( done ) {
+      sinon.stub( UserSession.prototype, "extend" )
+        .callsFake( function ( ) {
+          this.test_groups = ["helpful-id-tips-reviewer", "helpful-id-tips"];
+        } );
+      request( this.app ).get( "/v2/exemplar_identifications" )
+        .set( "Authorization", nonAdminToken )
+        .set( "Content-Type", "application/json" )
+        .expect( 200, ( ) => {
+          UserSession.prototype.extend.restore( );
+          done( );
+        } );
+    } );
+
     it( "returns JSON", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -25,7 +75,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can include include category_counts", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=3&include_category_counts=true" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -36,7 +86,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can include include category_controlled_terms", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?include_category_controlled_terms=true" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -47,7 +97,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by upvoted", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=3&upvoted=true&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -60,7 +110,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by downvoted", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=3&downvoted=true&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -73,7 +123,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by nominated", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=7&nominated=true&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -86,7 +136,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by not nominated", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=7&nominated=false&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -99,7 +149,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by query", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=7&q=unnominated&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
@@ -112,7 +162,7 @@ describe( "ExemplarIdentifications", ( ) => {
 
     it( "can filter by term_value_id", function ( done ) {
       request( this.app ).get( "/v2/exemplar_identifications?direct_taxon_id=3&term_value_id=2&fields=all" )
-        .set( "Authorization", token )
+        .set( "Authorization", adminToken )
         .set( "Content-Type", "application/json" )
         .expect( "Content-Type", /json/ )
         .expect( res => {
