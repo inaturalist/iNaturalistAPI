b1f0760004c66beeefb598e68293ad7eed58ac59
Patrick Leary <patrick.r.leary@gmail.com>
2026-07-29 10:57:17 -0400

    fix: v2 users projects endpoint properly consistently handles pagination, WEB-1103

--- openapi/paths/v2/users/{id}/projects.js
+++ openapi/paths/v2/users/{id}/projects.js
@@ -1,6 +1,8 @@
+const _ = require( "lodash" );
 const Joi = require( "joi" );
 const transform = require( "../../../../joi_to_openapi_parameter" );
 const UsersController = require( "../../../../../lib/controllers/v1/users_controller" );
+const usersProjectsSchema = require( "../../../../schema/request/users_projects" );
 
 module.exports = sendWrapper => {
   async function GET( req, res ) {
@@ -8,23 +10,30 @@ module.exports = sendWrapper => {
     sendWrapper( req, res, null, results );
   }
 
+  const parameters = [
+    transform(
+      Joi.number( ).integer( )
+        .label( "id" )
+        .meta( { in: "path" } )
+        .required( )
+        .description( "A single project ID" )
+    )
+  ].concat(
+    _.map( usersProjectsSchema.$_terms.keys, child => (
+      transform( child.schema.label( child.key ) )
+    ) )
+  );
+  parameters.push(
+    transform( Joi.string( ).label( "X-HTTP-Method-Override" ).meta( { in: "header" } ) )
+  );
+
   GET.apiDoc = {
     tags: ["Users"],
     summary: "Return projects as user has joined / followed.",
     security: [{
       userJwtOptional: []
     }],
-    parameters: [
-      transform(
-        Joi.number( ).integer( )
-          .label( "id" )
-          .meta( { in: "path" } )
-          .required( )
-          .description( "A single project ID" )
-      ),
-      transform( Joi.string( ).label( "fields" ).meta( { in: "query" } ) ),
-      transform( Joi.string( ).label( "X-HTTP-Method-Override" ).meta( { in: "header" } ) )
-    ],
+    parameters,
     responses: {
       200: {
         description: "An array of projects.",
